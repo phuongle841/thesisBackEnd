ef00bb3e5ac49e65ba4c9f2e92fda1932c6955fe
leMinhPhuong <phuongleminh841@gmail.com>
2025-02-11 22:19:09 +0700

    feat(db):connect

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const cors = require("cors");
+
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use(cors());
+app.use(bodyParser.json());
 
-const PORT = 3000;
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+app.get("/users", (req, res) => {
+  return res.send(users);
+});
+
+app.get("/", (req, res) => {
+  return res.send("Received a GET HTTP method");
+});
+
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
+});
+
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
 });
